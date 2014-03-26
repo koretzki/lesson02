e5488eb86897c12d8f29e0b32a01bc5532a6b08d
ran koretzki <rankoretzki@gmail.com>
2014-03-26 10:54:55 +0200

    polindrom created

---
+++ polyndrom.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ranko on 26/03/14.
+ */
+function isPolyndrom(word) {
+    var l = word.length;
+    for (var i = 0; i < l; i++) {
+        if (word.charAt(i) !== word.charAt(l - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log('result is', isPolyndrom('aa'))
